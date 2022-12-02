3d2993ee02a26a238a0780a7a5bb869a26bd0a52
nehernan <hernandezneilen@gmail.com>
2022-12-02 11:05:20 -0300

    stars animations and fix navbar

--- js/navbar.js
+++ js/navbar.js
@@ -7,11 +7,15 @@ var navbar = document.getElementById("navbar");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
+
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
 function myFunction() {
     if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
+        navbar.classList.add("fixed");
+        navbar.classList.remove("bgcTransparent");
     } else {
-        navbar.classList.remove("sticky");
+        navbar.classList.add("bgcTransparent");
+        navbar.classList.remove("fixed");
     }
 }
